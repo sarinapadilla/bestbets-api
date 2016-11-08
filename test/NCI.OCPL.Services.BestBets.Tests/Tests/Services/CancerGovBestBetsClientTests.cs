2173ba684f9d87a2bdd89d96949a8265687339b0
Bryan Pizzillo <bryanp@mail.nih.gov>
2016-11-08 17:20:49 -0500

    Mocking Client Tests

--- test/NCI.OCPL.Services.BestBets.Tests/Tests/Services/CancerGovBestBetsClientTests.cs
+++ test/NCI.OCPL.Services.BestBets.Tests/Tests/Services/CancerGovBestBetsClientTests.cs
@@ -2,22 +2,35 @@ using System.Net.Http;
 
 using Xunit;
 using Moq;
+using RichardSzalay.MockHttp;
+
+using NCI.OCPL.Utils.Testing;
 
 namespace NCI.OCPL.Services.BestBets.Tests
 {
     public class CancerGovBestBetsClientTests
     {
         
+        [Fact]
+        public void GetBestBetForDisplay()
+        {
+            //Setup a mock handler, which is what HttpClient uses under the hood to fetch
+            //data.
+            var mockHttp = new MockHttpMessageHandler();
 
+            string filePath = "";
 
+            ByteArrayContent content = new ByteArrayContent(TestingTools.GetTestFileAsBytes(filePath));
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/xml");
 
+            mockHttp
+                .When("https://www.cancer.gov/PublishedContent/BestBets/36408.xml")
+                .Respond(System.Net.HttpStatusCode.OK, content);
 
-        /// <summary>
-        /// Class for mocking responses requested by HttpClient for tests
-        /// </summary>
-        class TestHttpMessageHandler : HttpMessageHandler {
+            
 
-        } 
+        }
+
 
     }
 }

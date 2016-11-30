233fcfae66982c42aea7fb34423f81e8e97868bd
Bryan Pizzillo <bryanp@mail.nih.gov>
2016-11-30 11:48:19 -0500

    Adding skeleton for PublishedContentListing service.

---
+++ src/NCI.OCPL.Services.CDE.PublishedContentListing/CDEPubContentListingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NCI.OCPL.Services.CDE.PublishedContentListing
+{
+    public class CDEPubContentListingService : IPublishedContentListingService
+    {
+        public CDEPubContentListingService()
+        {
+        }
+    }
+}
---
+++ src/NCI.OCPL.Services.CDE.PublishedContentListing/IPublishedContentListingService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NCI.OCPL.Services.CDE
+{
+    interface IPublishedContentListingService
+    {
+    }
+}

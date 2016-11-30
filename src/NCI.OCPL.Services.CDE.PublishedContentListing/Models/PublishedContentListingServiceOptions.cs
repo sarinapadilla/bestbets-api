8b3c3f6073b7371d30ec58b10c1039fb6cfab6cb
Blair Learn <blairlearn@users.noreply.github.com>
2016-11-30 17:08:01 -0500

    Stub unit tests for CDEPubContentListingService

--- src/NCI.OCPL.Services.CDE.PublishedContentListing/Models/PublishedContentListingServiceOptions.cs
+++ src/NCI.OCPL.Services.CDE.PublishedContentListing/Models/PublishedContentListingServiceOptions.cs
@@ -12,5 +12,16 @@ namespace NCI.OCPL.Services.CDE.PublishedContentListing
         /// </summary>
         /// <returns></returns>
         public string Host { get; set; }
+
+        /// <summary>
+        /// This is the root path for retrieving lists.
+        /// </summary>
+        public string ListRoot { get; set; }
+
+        /// <summary>
+        /// This is the path to use when retrieving the list of files
+        /// for a specific list and path
+        /// </summary>
+        public string SpecificListPathFormatter { get; set; }
     }
 }
---
+++ test/NCI.OCPL.Services.CDE.Tests/Tests/CDEPubContentListingService.cs
@@ -0,0 +1,19 @@
+namespace NCI.OCPL.Services.CDE.Tests.CDEPubContentListingServiceTests
+{
+    /// <summary>
+    /// Tests for CDEPubContentListingService.GetItemsForPath()
+    /// </summary>
+    public class GetItemsForPath
+    {
+
+    }
+
+    /// <summary>
+    /// Tests for CDEPubContentListingService.GetItemsForPath()
+    /// </summary>
+    public class ListAvailablePaths
+    {
+
+    }
+
+}

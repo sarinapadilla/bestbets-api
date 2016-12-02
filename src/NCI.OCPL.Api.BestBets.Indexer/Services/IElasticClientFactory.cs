4a5a623b7050aa20ba5b5d481cebe03886dd6eaf
Blair Learn <blairlearn@users.noreply.github.com>
2016-12-02 15:29:27 -0500

    Refactor loading of configuration data.

---
+++ src/NCI.OCPL.Api.BestBets.Indexer/Services/IElasticClientFactory.cs
@@ -0,0 +1,13 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NCI.OCPL.Api.BestBets.Indexer.Services
+{
+    public interface IElasticClientFactory
+    {
+        IElasticClient GetInstance();
+    }
+}

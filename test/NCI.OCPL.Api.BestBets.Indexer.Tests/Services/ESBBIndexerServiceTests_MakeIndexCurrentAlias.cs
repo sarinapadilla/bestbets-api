ae24a6864fb3a3bb051181b36b84a1459e4e30f8
Bryan Pizzillo <bryanp@mail.nih.gov>
2016-12-05 17:27:28 -0500

    Adding in stubs for ESBBIndexerService Tests

---
+++ test/NCI.OCPL.Api.BestBets.Indexer.Tests/Services/ESBBIndexerServiceTests_MakeIndexCurrentAlias.cs
@@ -0,0 +1,26 @@
+﻿
+using Elasticsearch.Net;
+using Nest;
+
+using Xunit;
+using Moq;
+
+using System;
+using System.Threading.Tasks;
+
+using NCI.OCPL.Utils.Testing;
+using NCI.OCPL.Api.BestBets.Indexer.Services;
+using Microsoft.Extensions.Options;
+using NCI.OCPL.Api.BestBets.Tests.Util;
+
+namespace NCI.OCPL.Api.BestBets.Indexer.Tests
+{
+    public class ESBBIndexerServiceTests_MakeIndexCurrentAlias
+    {
+        [Fact]
+        public void TEST()
+        {
+            
+        }
+    }
+}

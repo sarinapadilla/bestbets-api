0745d225ef4ad237cbe68cee2445b0739b107680
Blair Learn <blairlearn@users.noreply.github.com>
2016-12-09 14:43:14 -0500

    Unit tests for index creation.
    MakeCurrentNoExisting - create index when no previous existing index.
    MakeCurrentOneExisting - create index when a previous index does exst.

--- test/NCI.OCPL.Api.BestBets.Indexer.Tests/Services/ESBBIndexerServiceTests_MakeIndexCurrentAlias.cs
+++ test/NCI.OCPL.Api.BestBets.Indexer.Tests/Services/ESBBIndexerServiceTests_MakeIndexCurrentAlias.cs
@@ -20,6 +20,9 @@ namespace NCI.OCPL.Api.BestBets.Indexer.Tests
 {
     public class ESBBIndexerServiceTests_MakeIndexCurrentAlias : ESBBIndexerServiceTests__Base
     {
+        /// <summary>
+        /// Verify that a single index is created when there is no pre-existing index for the alias.
+        /// </summary>
         [Fact]
         public void MakeCurrentNoExisting()
         {
@@ -27,7 +30,7 @@ namespace NCI.OCPL.Api.BestBets.Indexer.Tests
             string newIndexName = aliasName + "20161207125500";
 
             string actualPath = string.Empty;
-            object actualRequestOptions = null;
+            JObject actualRequestOptions = null;
 
             ESBBIndexerService service = this.GetIndexerService(
                 aliasName,
@@ -57,20 +60,59 @@ namespace NCI.OCPL.Api.BestBets.Indexer.Tests
 
             //Make sure the Request is the expected request
             //Assert.Equal("_alias/" + aliasName, actualPath);
-            //Assert.Null(actualRequestOptions);
+            string[] expectedAddIndices = new string[] { newIndexName };
+            string[] expectedRemoveIndices = new string[] { };
+
+            // Pull out the lists of aliases and indexes being added/removed.
+            string[] actualAddedAliases =
+                (from act in actualRequestOptions["actions"]
+                 where act["add"] != null
+                 select (string)act["add"]["alias"]).ToArray();
+
+            string[] actualAddedIndices =
+                (from act in actualRequestOptions["actions"]
+                 where act["add"] != null
+                 select (string)act["add"]["index"]).ToArray();
+            
+            // Items where the add action is missing, or remove is present.  (List should be empty.)
+            var unexpectedList =
+                from act in actualRequestOptions["actions"]
+                where act["add"] == null || act["remove"] != null
+                select act;
+
+            // Check list lengths.
+            bool correctListSizes =
+                actualAddedAliases.Length == 1
+                && actualAddedIndices.Length == 1
+                && unexpectedList.Count() == 0;
+
+            // Check that all expected indices are accounted for.
+            bool expectedAddIndicesPresent = expectedAddIndices.All(index => actualAddedIndices.Contains(index));
+
+            // All aliases should match aliasName.
+            bool addAliasCorrect = actualAddedAliases.All(alias => alias == aliasName);
 
             //Are assertions will be on the actual request options.
-            Assert.True(false);            
+            Assert.True(
+                correctListSizes
+                && expectedAddIndicesPresent
+                && addAliasCorrect
+                );
 
         }
 
+        /// <summary>
+        /// Verify that a single index is created when a single index already exists and that
+        /// all pre-existing indices for the alias are removed.
+        /// </summary>
+        [Fact]
         public void MakeCurrentOneExisting()
         {
             string aliasName = "bestbets";
             string newIndexName = aliasName + "20161207125500";
 
             string actualPath = string.Empty;
-            object actualRequestOptions = null;
+            JObject actualRequestOptions = null;
 
             ESBBIndexerService service = this.GetIndexerService(
                 aliasName,
@@ -114,13 +156,68 @@ namespace NCI.OCPL.Api.BestBets.Indexer.Tests
 
             //Make sure the Request is the expected request
             //Assert.Equal("_alias/" + aliasName, actualPath);
-            //Assert.Null(actualRequestOptions);
+            // Values for expectedRemoveIndices MUST match GetIndicesForAlias_SingleItemResponse.json
+            string[] expectedAddIndices = new string[] { newIndexName };
+            string[] expectedRemoveIndices = new string[] { "bestbets20161202152737" };
+
+            // Pull out the lists of aliases and indexes being added/removed.
+            string[] actualAddedAliases =
+                (from act in actualRequestOptions["actions"]
+                 where act["add"] != null
+                 select (string)act["add"]["alias"]).ToArray();
+
+            string[] actualAddedIndices =
+                (from act in actualRequestOptions["actions"]
+                 where act["add"] != null
+                 select (string)act["add"]["index"]).ToArray();
+
+            string[] actualRemovedAliases =
+                (from act in actualRequestOptions["actions"]
+                 where act["remove"] != null
+                 select (string)act["remove"]["alias"]).ToArray();
+
+            string[] actualRemovedIndices =
+                (from act in actualRequestOptions["actions"]
+                 where act["remove"] != null
+                 select (string)act["remove"]["index"]).ToArray();
+
+            // Items where both actions are missing, or both are present.  (List should be empty.)
+            var unexpectedList =
+                from act in actualRequestOptions["actions"]
+                where (act["add"] != null && act["remove"] != null) || (act["add"] == null && act["remove"] == null)
+                select act;
+
+            // Check list lengths.
+            bool correctListSizes =
+                actualAddedAliases.Length == 1
+                && actualAddedIndices.Length == 1
+                && actualRemovedAliases.Length == 1
+                && actualRemovedIndices.Length == 1
+                && unexpectedList.Count() == 0;
+
+            // Check that all expected indices are accounted for.
+            bool expectedAddIndicesPresent = expectedAddIndices.All(index => actualAddedIndices.Contains(index));
+            bool expectedRemoveIndicesPresent = expectedRemoveIndices.All(index => actualRemovedIndices.Contains(index));
+
+            // All aliases should match aliasName.
+            bool addAliasCorrect = actualAddedAliases.All(alias => alias == aliasName);
+            bool removeAliasCorrect = actualRemovedAliases.All(alias => alias == aliasName);
 
             //Are assertions will be on the actual request options.
-            Assert.True(false);            
+            Assert.True(
+                correctListSizes
+                && expectedAddIndicesPresent
+                && expectedRemoveIndicesPresent
+                && addAliasCorrect
+                && removeAliasCorrect
+                );
 
         }
 
+        /// <summary>
+        /// Verify that a single index is created when multiple indices exist and that
+        /// all pre-existing indices for the alias are removed.
+        /// </summary>
         [Fact]
         public void MakeCurrentManyExisting()
         {
@@ -173,6 +270,7 @@ namespace NCI.OCPL.Api.BestBets.Indexer.Tests
             // Make sure the Request is the expected request
             // Index being added should be newIndexName
             // Alias being addded should be aliasName
+            // Values for expectedRemoveIndices MUST match GetIndicesForAlias_MultiItemResponse.json
             string[] expectedAddIndices = new string[] { newIndexName };
             string[] expectedRemoveIndices = new string[] { "bestbets20161202152737", "bestbets20161201165226" };
 

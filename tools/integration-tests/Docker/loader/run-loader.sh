1520ae143e13d0d6fe3a8bd4ea35dda3a13d2fe9
Bryan Pizzillo <bryanp@mail.nih.gov>
2018-08-07 16:43:00 -0400

    Got initial API + Data working.

--- tools/integration-tests/Docker/loader/run-loader.sh
+++ tools/integration-tests/Docker/loader/run-loader.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
 
-curl cancergov
+is_ready() {
+    [ $(curl --write-out %{http_code} --silent --output /dev/null http://elasticsearch:9200/_cat/health?h=st) = 200 ] \
+    && [ $(curl --write-out %{http_code} --silent --output /dev/null https://cancergov/PublishedContent/List) = 200 ] \
+    && [ $(curl --write-out %{http_code} --silent --output /dev/null https://preview-cancergov/PublishedContent/List) = 200 ]
+}
+
+# wait until is ready
+i=0
+while ! is_ready; do
+    i=`expr $i + 1`
+    if [ $i -ge 10 ]; then
+        echo "$(date) - still not ready, giving up"
+        exit 1
+    fi
+    echo "$(date) - waiting to be ready"
+    sleep 10
+done
+
+#start the script
+echo "Loading Live..."
+NODE_TLS_REJECT_UNAUTHORIZED=0 BBSOURCEHOST=cancergov BBALIAS=bestbets_live_v1 node index.js
+echo "Loading Preview..."
+NODE_TLS_REJECT_UNAUTHORIZED=0 BBSOURCEHOST=preview-cancergov BBALIAS=bestbets_preview_v1 node index.js
+tail -f /dev/null
